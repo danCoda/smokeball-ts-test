52cce4ca7ed1568dda05cfdfdcf0eaf012579c21
Daniel Son <dson.email@gmail.com>
2022-09-28 15:52:11 +1000

    Get Arnie quotes and pass all tests

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,65 @@
-import { httpGet } from './mock-http-interface';
+import { httpGet } from "./mock-http-interface";
 
-// TODO define this type properly
-type TResult = any;
-
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
+type SuccessResult = {
+  "Arnie Quote": string;
 };
+
+type FailureResponse = {
+  FAILURE: string;
+};
+
+type TResult = SuccessResult | FailureResponse;
+
+type GetResponse = {
+  status: number;
+  body: string;
+};
+
+const getQuote = async (url: string) => {
+  const response = (await httpGet(url)) as GetResponse;
+  const body = JSON.parse(response.body);
+
+  switch (response.status) {
+    case 200:
+      return {
+        "Arnie Quote": body.message,
+      } as SuccessResult;
+
+    default:
+      return {
+        FAILURE: body.message,
+      } as FailureResponse;
+  }
+};
+
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
+  const quotes = await urls.map((url) => getQuote(url));
+
+  return Promise.all(quotes);
+};
+
+/* return new Promise(async (resolve, reject) => {
+    const results: TResult[] = [];
+
+    while (numUrls--) {
+      const response = (await httpGet(urls[i])) as GetResponse;
+
+      const body = JSON.parse(response.body);
+
+      let result: TResult = {};
+
+      if (response.status === 200) {
+        result = {
+          "Arnie Quote": body.message,
+        };
+      } else {
+        result = {
+          FAILURE: body.message,
+        };
+
+        results.push(result);
+      }
+    }
+
+    //resolve(results);
+  }); */
--- src/mock-http-interface.ts
+++ src/mock-http-interface.ts
@@ -23,6 +23,8 @@ export const httpGet = async (url : string) => {
     return { status: 200, body: JSON.stringify({ message }) };
   } 
   catch (err) {
-    return { status: 500, body: JSON.stringify({ message: err.message }) };
+    if (err instanceof Error) {
+      return { status: 500, body: JSON.stringify({ message: err.message }) };
+    }
   }
 };
